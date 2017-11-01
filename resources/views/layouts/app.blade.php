da5f748056465b479b40bff99fccada0e0c4db52
mwakaambrose <ambrosemwaka@gmail.com>
2017-11-01 22:05:44 +0300

    Remakes UI

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,74 +10,48 @@
     <title>{{-- {{ env('APP_NAME', 'ambrose.pro') }} --}} ambrose.pro</title>
     <!-- Styles -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/style.css') }}" rel='stylesheet' type='text/css' />
+    <link rel="stylesheet" href="css/main.css"/>
+    <link rel="stylesheet" href="css/font-awesome.min.css" />
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
+        <!-- Header -->
+        <header id="header">
+            <nav class="left">
+                <a href="#menu"><span>Menu</span></a>
+                {{-- <a href="#menu"><span>Menu</span></a> --}}
+            </nav>
+            <a href="/" class="logo">ambrose.pro</a>
+            <nav class="right">
+                <a href="#" class="button alt">Pricing</a>
+                {{-- <a href="#" class="button alt">Pricing</a> --}}
+            </nav>
+        </header>
 
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        <strong>ambrose</strong>.pro
-                    </a>
-                </div>
-
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @if (Auth::guest())
-                            {{-- <li><a href="{{ route('login') }}">Clients Login</a></li> --}}
-                            {{-- <li><a href="{{ route('register') }}">Clients Register</a></li> --}}
-                            <li><a href="/my-work">My Work</a></li>
-                            <li><a href="/more-information"><strong>Learn More</strong></a></li>
-                            <li><a href="/pricing"><strong>Pricing</strong></a></li>
-                            <li><a href="/tutorials"><strong>Tutorials</strong></a></li>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <ul class="dropdown-menu" role="menu">
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endif
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        <!-- Menu -->
+        {{-- <nav id="menu"> --}}
+            {{-- <ul class="links">
+                <li><a href="index.html">Home</a></li>
+                <li><a href="generic.html">Generic</a></li>
+                <li><a href="elements.html">Elements</a></li>
+            </ul> --}}
+            {{-- <ul class="actions vertical">
+                <li><a href="#" class="button fit">Pricing</a></li>
+            </ul> --}}
+        {{-- </nav> --}}
 
         @yield('content')
 
     </div>
-    <!-- Scripts -->
+    <!-- Footer -->
+    {{-- <div class="copyright">
+        &copy 2017 ambrose.pro  TrustFinity Developer
+    </div> --}}
+    <!-- Scripts-->
     <script src="{{ mix('js/app.js') }}"></script>
+    <script src="js/jquery.scrolly.min.js"></script>
+    {{-- <script src="js/skel.min.js"></script> --}}
+    {{-- <script src="js/util.js"></script> --}}
+    <script src="js/main.js"></script>
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,50 +1,65 @@
 @extends('layouts.app')
 @section('body-class', 'welcome')
-
 @section('content')
-<div class="header">
-   <div class="header_bot_grid">
-      <h1>Personal and Business Cloud App Development</h1>
+
+<!-- Banner -->
+<section id="banner">
+    <div class="content">
+        <h1>Personal and Business Cloud</h1>
         <div id="text-carousel" class="carousel slide" data-ride="carousel">
             <div class="col-xs-offset-3 col-xs-6">
                 <div class="carousel-inner">
                     <div class="item active">
                         <div class="carousel-content">
                             <div>
-                                <p>I will host your website for as low as <strong>UGX 10,000</strong>. Save <strong>20%</strong> on Yearly Plan.</p>
+                                <p>I will host your website for as low as <strong>UGX 10,000</strong>. Save 
+                                    <strong>20%</strong> on Yearly Plans.</p>
                             </div>
                         </div>
                     </div>
                     <div class="item">
                         <div class="carousel-content">
                             <div>
-                                <p>A managed <strong>Laravel</strong> app hosting for <strong>UGX 20,000/mo</strong> on a 512mb VPS</p>
+                                <p>A managed <strong>Laravel</strong> app hosting for 
+                                    <strong>UGX 55,000/mo</strong>.</p>
                             </div>
                         </div>
                     </div>
                     <div class="item">
                         <div class="carousel-content">
                             <div>
-                                <p>I will build a cloud system for your bussiness for as low a <strong>UGX 2 million.</strong></p>
+                                <p>We will build a cloud applocation for your bussiness for as low a 
+                                    <strong>UGX 2 million.</strong></p>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-      <span></span>
-   </div>
-</div>
-
-<div class="footer">
-    <div class="container">
-        <div class="col-md-4">
-        </div>
-        <div class="col-md-4">
-            <p>&copy 2017 ambrose.pro a <strong>TrustFinity </strong>Developer</p>
-        </div>
-        <div class="col-md-4">
-        </div>
     </div>
-</div>
+</section>
+
+<!-- Two -->
+<section id="two" class="wrapper style1 special">
+    <div class="inner">
+        <h2>Launch your business in the cloud today</h2>
+        <figure>
+            <blockquote>
+                "Secure, global, high-performance, cost-effective and constantly improving. Our cloud <br>service is for the long haul."
+            </blockquote>
+            <footer>
+                <cite class="author">Ambrose Mwaka</cite>
+                <cite class="company">CTO, TrustFinity</cite>
+            </footer>
+        </figure>
+        <br>
+        <p>&copy 2017 ambrose.pro  TrustFinity Developer</p>
+    </div>
+</section>
+
+<!-- Three -->
+{{-- <section id="three" class="wrapper">
+    <div class="inner flex flex-3">
+    </div>
+</section> --}}
 @endsection

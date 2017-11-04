11c798e84bf8ebb155a4c3f98258fac2717d3434
mwakaambrose <ambrosemwaka@gmail.com>
2017-11-04 19:07:24 +0300

    Page visit Analytics

--- resources/views/mail/welcome.blade.php
+++ resources/views/mail/welcome.blade.php
@@ -1,15 +1,6 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
   <head>
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-109209789-1"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', 'UA-109209789-1');
-    </script>
-
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>Go Ping Email</title>
